2fcb14f64e7116ecc5d0ab417f0332cb4325ddb3
shirkhanfg-mpa201 <shirkhanfg-mpa201@code.edu.az>
2025-10-29 21:17:16 +0400

    Assignment

---
+++ RestaurantAssignment/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAssignment;
+
+public class Order
+{
+
+    public int Id { get; set; }
+    public Dish[] dishes { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public Order(int id)
+    {
+        Id = id;
+    }
+
+
+}
---
+++ RestaurantAssignment/Restaurant.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAssignment;
+
+public class Restaurant
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public Order[] Orders { get; set; }
+    public Dish[] Menu { get; set; }
+
+    public Restaurant(string name)
+    {
+        Name = name;
+        Menu = new Dish[0];     
+        Orders = new Order[0];
+    }
+
+    public void AddDishToMenu(Dish dish)
+    {
+        Dish[] newMenu = new Dish[Menu.Length + 1];
+        for (int i = 0; i < Menu.Length; i++)
+        {
+            newMenu[i] = Menu[i];
+        }
+        newMenu[Menu.Length] = dish;
+        Menu = newMenu;
+    }
+
+    public void PlaceOrder(Order order)
+    {
+        Order[] newOrders = new Order[Orders.Length + 1];
+        for (int i = 0; i < Orders.Length; i++)
+        {
+            newOrders[i] = Orders[i];
+        }
+        newOrders[Orders.Length] = order;
+        Orders = newOrders;
+    }
+    public Order GetOrderById(int id)
+    {
+        for (int i = 0; i < Orders.Length; i++)
+        {
+            if (Orders[i].Id == id)
+                return Orders[i];
+        }
+        return null; 
+    }
+
+}
